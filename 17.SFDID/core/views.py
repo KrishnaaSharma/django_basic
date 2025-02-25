dd39b2d45f4b6a7335d9f56b594ffaec07f19063
KrishnaaSharma <krishnaasharma647@gmail.com>
2025-02-25 10:16:46 +0530

    Update

--- 17.SFDID/core/views.py
+++ 17.SFDID/core/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render,redirect
 from .forms import MarvelForm
+from .models import MarvelModel
+
 # Create your views here.
 def index(request):
     if request.method == 'POST':
         mf = MarvelForm(request.POST)
         if mf.is_valid():
-            print(f"Name is {mf.cleaned_data['name']}")
-            print(f"Heoric Name is {mf.cleaned_data['heroic_name']}")
+            nm= mf.cleaned_data['name']
+            hn= mf.cleaned_data['heroic_name']
+            MarvelModel(name=nm,heroic_name=hn).save()
             return redirect('/')
     else:
         mf=MarvelForm()
